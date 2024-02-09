a810d723b1a6300feeeda3f4119ae2bd35ebfd53
GoletianiZura <159282740+GoletianiZura@users.noreply.github.com>
2024-02-09 19:22:18 +0400

    Add files via upload

---
+++ error_code.py
@@ -0,0 +1,25 @@
+1.	#print(“hello, world”)
+2.	1user = zura
+3.	London = cityს
+4.	Print(name)
+5.	#city = Berlin
+6.	name= giga
+7.	print(“name” + 35)
+8.	user-1 = john
+9.	input(how are you: “)
+10.	print(namE)
+11.	salary = 120 + name
+12.	name_1 = 120
+13.	sum = 60 + 50 print(“sum”)
+14.	i_know_programmin = false
+15.	age = “30”
+16.	30 + name
+17.	size = medium
+18.	day_count = ‘35’
+19.	name = “John’
+20.	$ keep in mind to change name
+21.	song = Midnight
+22.	#score = 15 print(score)
+23.	print(name(
+24.	name_1 = “john” print(john)
+25.	2_car = “audi”
